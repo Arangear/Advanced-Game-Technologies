ec1097eb8a39c3ac82ffe8e25ec7b213b5e62085
Daniel Cieslowski <D.Cieslowski2@newcastle.ac.uk>
2019-12-05 15:03:54 +0000

    Added tutorial 6

--- CSC8503/GameTech/Main.cpp
+++ CSC8503/GameTech/Main.cpp
@@ -15,7 +15,48 @@
 using namespace NCL;
 using namespace CSC8503;
 
-void TestStateMachine() {
+void TestStateMachine()
+{
+	StateMachine* testMachine = new StateMachine();
+	
+	int someData = 0;
+	
+	StateFunc AFunc = [](void* data)
+	{
+		int* realData = (int*)data;
+		(*realData)++;
+		std::cout << "In State A!" << std::endl;
+	};
+	StateFunc BFunc = [](void* data)
+	{
+		int* realData = (int*)data;
+		(*realData)--;
+		std::cout << "In State B!" << std::endl;
+		};
+	
+	GenericState* stateA = new GenericState(AFunc, (void*)&someData);
+	GenericState* stateB = new GenericState(BFunc, (void*)&someData);
+	testMachine->AddState(stateA);
+	testMachine->AddState(stateB);
+
+	GenericTransition<int&, int>* transitionA =
+			new GenericTransition<int&, int>(
+			GenericTransition<int&, int>::GreaterThanTransition,
+			someData, 10, stateA, stateB); // if greater than 10 , A to B
+	
+	GenericTransition<int&, int>* transitionB =
+			new GenericTransition<int&, int>(
+			GenericTransition<int&, int>::EqualsTransition,
+			someData, 0, stateB, stateA); // if equals 0 , B to A
+	
+	testMachine->AddTransition(transitionA);
+	testMachine->AddTransition(transitionB);
+	
+	for (int i = 0; i < 100; ++i)
+	{
+		testMachine->Update(); // run the state machine!
+	}
+	delete testMachine;
 }
 
 void TestNetworking() {
@@ -52,7 +93,7 @@ int main() {
 		return -1;
 	}	
 
-	//TestStateMachine();
+	TestStateMachine();
 	//TestNetworking();
 	//TestPathfinding();
 	
