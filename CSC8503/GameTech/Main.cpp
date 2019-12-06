7846b3990668b0b0cb7e92104117f7e74b99b6b9
Daniel Cieslowski <D.Cieslowski2@newcastle.ac.uk>
2019-12-06 13:58:39 +0000

    Added tutorial 8

--- CSC8503/GameTech/Main.cpp
+++ CSC8503/GameTech/Main.cpp
@@ -59,7 +59,59 @@ void TestStateMachine()
 	delete testMachine;
 }
 
-void TestNetworking() {
+class TestPacketReceiver : public PacketReceiver
+{
+public:
+	TestPacketReceiver(string name)
+	{
+		this->name = name;
+	}
+	
+	void ReceivePacket(int type, GamePacket* payload, int source)
+	{
+		if (type == String_Message)
+		{
+			StringPacket* realPacket = (StringPacket*)payload;
+			
+			string msg = realPacket->GetStringFromData();
+			
+			std::cout << name << " received message: " << msg << std::endl;
+		}
+	}
+protected:
+	string name;
+};
+
+
+void TestNetworking()
+{
+	NetworkBase::Initialise();
+	
+	TestPacketReceiver serverReceiver("Server");
+	TestPacketReceiver clientReceiver("Client");
+	
+	int port = NetworkBase::GetDefaultPort();
+	
+	GameServer* server = new GameServer(port, 1);
+	GameClient* client = new GameClient();
+	
+	server->RegisterPacketHandler(String_Message, &serverReceiver);
+	client->RegisterPacketHandler(String_Message, &clientReceiver);
+	
+	bool canConnect = client->Connect(127, 0, 0, 1, port);
+	
+	for (int i = 0; i < 100; ++i)
+	{
+		server->SendGlobalPacket(StringPacket("Server says hello!" + std::to_string(i)));
+		
+		client->SendPacket(StringPacket("Client says hello!" + std::to_string(i)));
+		
+		server->UpdateServer();
+		client->UpdateClient();
+		
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	NetworkBase::Destroy();
 }
 
 vector<Vector3> testNodes;
@@ -115,8 +167,8 @@ int main() {
 	}	
 
 	//TestStateMachine();
-	//TestNetworking();
-	TestPathfinding();
+	TestNetworking();
+	//TestPathfinding();
 	
 	w->ShowOSPointer(false);
 	w->LockMouseToWindow(true);
